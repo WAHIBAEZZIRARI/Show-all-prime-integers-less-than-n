17304a010f58e4b8ce9447cf9b2f0cd8728ff9df
WAHIBAEZZIRARI <146207125+WAHIBAEZZIRARI@users.noreply.github.com>
2023-10-18 17:28:20 +0100

    Add files via upload

---
+++ td 1.py
@@ -0,0 +1,12 @@
+def est_premier(num):
+    if num <= 1:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+n = int(input("Entrez un entier n : "))
+for i in range(2, n):
+    if est_premier(i):
+        print(i)
